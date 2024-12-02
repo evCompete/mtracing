6d298eafbb56c2021775be694c33c35d3bdcca99
EvanCompete <evan@competeservices.com>
2024-12-02 16:05:12 -0700

    css

--- src/App.js
+++ src/App.js
@@ -1,35 +1,30 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 
 function App() {
   return (
     <Router>
-      <Switch>
-        <Route exact path="/">
-          <div>
-            <h1>Welcome</h1>
-            <p>Missoula Osprey Racing</p>
-          </div>
-        </Route>
-        <Route path="/about">
-          <div>
-            <h1>About Us</h1>
-          </div>
-        </Route>
-      </Switch>
+      <Routes>
+        <Route
+          path="/"
+          element={
+            <div>
+              <h1>Welcome</h1>
+              <p>Missoula Osprey Racing</p>
+            </div>
+          }
+        />
+        <Route
+          path="/about"
+          element={
+            <div>
+              <h1>About Us</h1>
+            </div>
+          }
+        />
+      </Routes>
     </Router>
   );
 }
 
 export default App;
-{/* <div className="App">
-  <header>
-    <h1>Welcome </h1>
-  </header>
-  <main>
-    <p>Missoula Osprey Racing</p>
-  </main>
-  <footer>
-    <p>© 2024 Bonperworth Productions</p>
-  </footer>
-</div> */}
